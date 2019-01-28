1ca0c8d000174e17560b324e95b843c7a7811e7f
kev112 <werewaswas@live.com>
2019-01-27 19:12:13 -0800

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -9,11 +9,11 @@ const WIN_COMBINATIONS = [
     [2, 4, 6]
 ]
 
-var turn = 0
+var turn = 0;
 
-var player = () => turn % 2 == 0 ? "X" : "O" 
+var player = () => turn % 2 == 0 ? "X" : "O";
 
-var currentGame = 0
+var currentGame = 0;
 
 $(function () {
     attachListeners()
@@ -28,9 +28,9 @@ var attachListeners = () => {
 
 var tdClicked = () => {
     $("td").on("click", function () {
-    if (!$.text(this) && !checkWinner()) {
-        doTurn(this)
-    }
+        if (!$.text(this) && !checkWinner()) {
+            doTurn(this);
+        }
     })
 }
     
@@ -51,11 +51,11 @@ function checkWinner() {
     WIN_COMBINATIONS.forEach(function (combo) {
     if( td[combo[0]] == td[combo[1]] &&
         td[combo[1]] == td[combo[2]] &&
-        td[combo[0]] != ""){
-        let winnerToken = td[combo[0]]
-        winner = true;
-        setMessage(`Player ${winnerToken} Won!`)
-    }
+        td[combo[0]] != "") {
+            let winnerToken = td[combo[0]]
+            winner = true;
+            setMessage(`Player ${winnerToken} Won!`)
+        }
     });
     return winner;
 }
@@ -63,30 +63,32 @@ function checkWinner() {
 var doTurn = (square) => {
     updateState(square)
     turn++
+
     if (checkWinner() == true) {
-    saveGame()
-    resetBoard()
+        saveGame()
+        resetBoard()
+
     } else if (turn == 9) {
-    setMessage("Tie game.")
-    saveGame()
-    resetBoard()
+        setMessage("Tie game.")
+        saveGame()
+        resetBoard()
     }
 }
 
 let resetBoard = () => {
     $("td").empty()
-    turn = 0
-    currentGame = 0
+    turn = 0;
+    currentGame = 0;
 }
 
 var previousGames = () => {
     $("#games").empty()
     $.get("/games", function (data) {
-    if (data.data.length) {
-        data.data.forEach(function (game) {
-        gameState(game)
-        })
-    }
+        if (data.data.length) {
+            data.data.forEach(function (game) {
+                gameState(game);
+            })
+        }
     })
 }
 
@@ -95,52 +97,57 @@ var gameState = (game) => {
     $(`#game-${game.id}`).on('click',() => reloadGame(game.id))
 } 
 
-function reloadGame(id) {
+let reloadGame = (id) => {
     $.get(`/games/${id}`, function (game) {
-    let state = game.data.attributes.state
-    var n = 0
-    state.map(function (i) {
-        if (i == "X" || i == "O") {
-        n++
-        }
+        
+        let state = game.data.attributes.state;
+        let n = 0;
+
+        state.map(function (i) {
+            if (i == "X" || i == "O") {
+                n++;
+            }
     })
-    currentGame = game.data.id
-    turn = n
-    populateGame(id, state, turn )
+    currentGame = game.data.id;
+    turn = n;
+    populateGame(id, state, turn);
 
     })
 }
 
-var saveGame = () => { 
+let saveGame = () => { 
     var gameData;
-    let td = []
+    let td = [];
 
     $("td").text(function (index, text) { td.push(text) })
     gameData = { state: td };
-    if (currentGame != 0) { // if currentGame has already been set with save button
 
-    $.ajax({
-        type: 'PATCH',  //patch over already saved game
-        url: `/games/${currentGame}`,
-        data: gameData
-    });
-    } else {  // if currentGame is still 0 (Note: 0 is *not* truthy in JavaScript)
-    $.post('/games', gameData, function (game) {  // save game first time
-        // debugger
-        currentGame = game.data.id; // save game first time
-        $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
-        $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
-    });
+    if (currentGame != 0) { 
+
+        $.ajax({
+            type: 'PATCH',  
+            url: `/games/${currentGame}`,
+            data: gameData
+        });
+
+    } else {  
+        
+        $.post('/games', gameData, (game) => {  
+            currentGame = game.data.id; 
+            $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
+            $("#gameid-" + game.data.id).on('click', () => reloadGame(game.data.id));
+        });
     }
 }
 
 var populateGame = (id, state, t ) => {
-    turn = t 
-    let index = 0
+    turn = t; 
+    let index = 0;
+
     for (let y = 0; y < 3; y++) {
-    for (let x = 0; x < 3; x++) {
-        document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index]
-        index++
-    }
+        for (let x = 0; x < 3; x++) {
+            document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
+            index++;
+        }
     }
 }
